a90b5e1ca73e89c94d3343a93be0246aebd7ca64
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-04 18:21:07 +0200

    ese finito ma qualcosa non va

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("Benvenuto user");
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +57,64 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+const container = document.getElementById('container');
+
+posts.forEach((element) =>{
+
+    container.innerHTML += 
+    `<div class="post">
+        <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src= "${element.author.image}" alt= "${element.author.name}">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">"${element.author.name}"</div>
+                <div class="post-meta__time">${element.created}</div>
+            </div> 
+        </div>
+        </div>
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+                <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div> 
+        `
+})
+
+let likePost = [];
+
+function like(){
+    let likeButton = document.querySelectorAll(`.like-button.js-like-button`);
+    let likes = document.querySelectorAll('.js-likes-counter');
+    const jsLikeButton = document.querySelector(".like-button__label");
+
+    
+    for (let i = 0; i < posts.length; i++) {
+        likes[i].innerHTML = posts[i].likes;
+        likeButton[i].addEventListener(`click`, () => {
+
+            posts[i].likes++;
+            likes[i].innerHTML = posts[i].likes;
+            jsLikeButton.style.color = 'blue';
+            likePost.push(posts[i].id);
+            console.log(likePost)
+        })
+    }
+}
+
+like();
