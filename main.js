670611557ed38bb8f96e14cb3c7f36cf00440d6d
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-06 17:30:45 +0200

    fix code add comment and create function for picture name

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("Benvenuto user");
 
+//Array di oggetti di //posts
 const posts = [
     {
         "id": 1,
@@ -58,71 +59,107 @@ const posts = [
     }
 ];
 
+//ID container
+let container = document.getElementById('container');
 
-const container = document.getElementById('container');
+for (let i = 0; i < posts.length; i++) {
+    const currentPost = posts[i];
+    container = renderPost(currentPost)
+}
 
-posts.forEach((element) =>{
+
+// funzione che restituisce HTML dei vari post
+function renderPost(post) {
+
+    const { author, created, content, media, likes, id } = post;
 
     container.innerHTML += 
     `<div class="post">
         <div class="post__header">
         <div class="post-meta">                    
             <div class="post-meta__icon">
-                <img class="profile-pic" src= "${element.author.image}" alt= "${element.author.name}">
+                ${ PicImage(author)}
             </div>
             <div class="post-meta__data">
-                <div class="post-meta__author">"${element.author.name}"</div>
-                <div class="post-meta__time">${element.created}</div>
+                <div class="post-meta__author">"${author.name}"</div>
+                <div class="post-meta__time">${created}</div>
             </div> 
         </div>
         </div>
-        <div class="post__text">${element.content}</div>
+        <div class="post__text">${content}</div>
         <div class="post__image">
-                <img src="${element.media}" alt="">
+                <img src="${media}" alt="">
         </div>
         <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                        <a class="like-button  js-like-button" href="#" data-postid="${id}">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
                     </div>
                 </div> 
             </div> 
         `
-})
+        return container;
+}
 
+
+//Creo l'Array like per i post
 let likePost = [];
 
+//buttone
 const buttons = document.getElementsByClassName("like-button");
 
 for (let i = 0; i < buttons.length; i++) {
     const currentButton = buttons[i];
 
+    //creo la funzione del button click
     currentButton.addEventListener('click', function(e){
         e.preventDefault();
         const clickedClass = "like-button--liked";
         const contatori = document.getElementsByClassName("js-likes-counter");
 
+        //se la classe clicked non è presente
         if (!currentButton.classList.contains(clickedClass)) {
             currentButton.classList.contains(clickedClass);
             currentButton.classList.add(clickedClass);
+
             let likes = parseInt(contatori[i].innerText);
             likes++;
+
             contatori[i].innerText = likes;
             likePost.push(i)
             console.log(likePost)
         } else {
             currentButton.classList.remove(clickedClass);
             let likes = parseInt(contatori[i].innerText);
+
             likes--;
             contatori[i].innerText = likes;
         }
 
     })
     
+}
+
+//se non è presente l'immagine del profilo
+function PicImage(author) {
+    let container = '';
+
+    if (author.image != null) {
+        container = `<img class="profile-pic" src= "${author.image}" alt= "${author.name}">`
+    } else {
+        container = `<div class="profile-pic-default"><span>${ getIniziali(author.name) }</span></div>`;
+    }
+
+    return container;
+}
+
+//funzioni per le iniziali
+function getIniziali(autore) {
+    return autore[0] + autore.split(" ")[1][0];
 }
